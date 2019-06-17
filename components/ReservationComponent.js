aaad424ed85e4c1b444eb72a5522631c135af1a4
Daghan Altas <daghan@gmail.com>
2019-06-17 09:39:13 -0700

    Secure Store

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,17 +1,8 @@
 import React, { Component } from "react";
-import {
-  Text,
-  View,
-  ScrollView,
-  StyleSheet,
-  Picker,
-  Switch,
-  Button,
-  Modal,
-  Alert
-} from "react-native";
+import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal, Alert } from "react-native";
 import DatePicker from "react-native-datepicker";
 import * as Animatable from "react-native-animatable";
+import { Permissions, Notifications } from 'expo';
 
 class Reservation extends Component {
   constructor(props) {
@@ -28,6 +19,34 @@ class Reservation extends Component {
   static navigationOptions = {
     title: "Reserve Table"
   };
+
+  async obtainNotificationPermission() {
+    let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+    if (permission.status !== 'granted') {
+        permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            Alert.alert('Permission not granted to show notifications');
+        }
+    }
+    return permission;
+  }
+  
+  async presentLocalNotification(date) {
+    await this.obtainNotificationPermission();
+    Notifications.presentLocalNotificationAsync({
+        title: 'Your Reservation',
+        body: 'Reservation for '+ date + ' requested',
+        ios: {
+            sound: true
+        },
+        android: {
+            sound: true,
+            vibrate: true,
+            color: '#512DA8'
+        }
+    });
+  }  
+  
 
   toggleModal() {
     this.setState({ showModal: !this.state.showModal });
@@ -48,7 +67,10 @@ class Reservation extends Component {
             },
             {
                 text: 'OK',
-                onPress: () => this.resetForm()
+                onPress: () => {
+                  this.presentLocalNotification(this.state.date)
+                  this.resetForm()
+                }
             }
         ],
         { cancelable: false }
